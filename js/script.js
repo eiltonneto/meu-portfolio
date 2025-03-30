d8001564e51d7b1f49de3ec77ce869701ddbdd91
Eilton Neto <eiltonneto74@gmail.com>
2025-03-30 12:23:40 -0300

    :tada: Commit Inicial

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* Usar uma variável para guardar um valor */
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+menuHamburguer.addEventListener('click', () =>{
+
+toggleMenu();
+
+});
+
+function toggleMenu(){
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+
+    if(menuHamburguer.classList.contains('change')){
+        nav.style.display = 'block';
+    
+    }else{
+        nav.style.display = 'none';
+    }
+}
