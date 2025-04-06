36f2a42f48cd0a4f8462e9473701960690475f37
Eilton Neto <eiltonneto74@gmail.com>
2025-04-06 20:00:14 -0300

    Adiciona botão Read More, link de currículo, redes sociais e projetos em produção

--- js/script.js
+++ js/script.js
@@ -17,3 +17,50 @@ function toggleMenu(){
         nav.style.display = 'none';
     }
 }
+
+// função controladora dos botões dos boxes services
+function toggleText(id, event) {
+    const text = document.getElementById(id);
+    const btn = event.target;
+   
+    if (text.classList.contains("short")) {
+      text.classList.remove("short");
+      text.classList.add("expanded");
+      btn.innerText = "Read Less";
+    } else {
+      text.classList.remove("expanded");
+      text.classList.add("short");
+      btn.innerText = "Read More";
+    }
+  }
+
+  function toggleText2(id, event) {
+    const text = document.getElementById(id);
+    const btn = event.target;
+   
+    if (text.classList.contains("short")) {
+      text.classList.remove("short");
+      text.classList.add("expanded");
+      btn.innerText = "Read Less";
+    } else {
+      text.classList.remove("expanded");
+      text.classList.add("short");
+      btn.innerText = "Read More";
+    }
+  }
+
+  // Modal About
+const aboutBtn = document.querySelector(".about-btn");
+const aboutModal = document.querySelector(".about-modal");
+const aboutClose = document.querySelector(".about-modal-close");
+
+aboutBtn.onclick = (e) => {
+  e.preventDefault(); // impede o scroll ao clicar no link
+  aboutModal.classList.add("active");
+};
+
+aboutClose.onclick = () => {
+  aboutModal.classList.remove("active");
+};
+
+
